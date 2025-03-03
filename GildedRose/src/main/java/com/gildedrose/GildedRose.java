8a732c173ebabc2ff3c7ef0a4892d50d617246f6
Matias A <matias.amaglio6@etu.univ-lorraine.fr>
2025-03-03 09:41:00 +0100

    sortie backstage

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -16,30 +16,12 @@ class GildedRose {
     private void handleQuality(Item item) {
         if (item.name.equals("Aged Brie")) {
             handleBrie(item);
+        } else if (item.name.equals("Backstage passes")) {
+            handleBackstage(item);
         } else {
-            if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras")) {
+                    item.quality = item.quality - 1;
                 }
             }
 
@@ -55,8 +37,6 @@ class GildedRose {
                                 item.quality = item.quality - 1;
                             }
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
                 }
             }
@@ -72,6 +52,38 @@ class GildedRose {
                     item.quality = item.quality + 1;
                 }
             }
+        }
+    }
+
+    private void handleBackstage(Item item) {
+        if (item.name.equals("Backstage passes")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (item.name.equals("Backstage passes")) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+        if (!item.name.equals("Sulfuras")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (item.name.equals("Backstage passes")) {
+                item.quality = item.quality - item.quality;
+            }
+
         }
     }
 }
